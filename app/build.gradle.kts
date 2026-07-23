2eef4a97a248052d5a6bf7aa638093112f3862a8
Jose Manuel Oberreuter <jose.manuel.oberreuter@gmail.com>
2026-07-22 22:51:08 -0400

    chore: increment version code to 15 and version name to 1.2.1 for the upcoming release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.josem.monopulcro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.1.1"
+        versionCode = 15
+        versionName = "1.2.1"
     }
 
     buildTypes {
