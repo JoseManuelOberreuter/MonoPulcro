78dd1108c5c7e11b5897d6e2f24db33626849773
Jose Manuel Oberreuter <jose.manuel.oberreuter@gmail.com>
2026-08-04 23:38:18 -0400

    chore: increment version code to 24 and update version name to 1.2.8 for the upcoming release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.josem.monopulcro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.2.7"
+        versionCode = 24
+        versionName = "1.2.8"
     }
 
     buildTypes {
