a4160be6d6ef3df1b6c336da9d2ff1b01c28e915
Jose Manuel Oberreuter <jose.manuel.oberreuter@gmail.com>
2026-08-08 14:28:21 -0400

    chore: update version code to 29 and version name to 1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.josem.monopulcro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
-        versionName = "1.3.2"
+        versionCode = 29
+        versionName = "1.3.3"
     }
 
     buildTypes {
